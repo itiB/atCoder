2c8f6f439c2f5adca8ad9318f79f45dfafbe7801
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-05-16 10:23:04 +0900

    Refact abc167-d

--- abc167/d.rs
+++ abc167/d.rs
@@ -3,50 +3,30 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        mut k: i64,
-        a: [i64; n],
+        mut k: usize,
+        a: [usize; n],
     }
 
-    let mut looped = vec![false; n];
-    looped[0] = true;
-    let mut go: i64 = 1;
-    let mut count = 0;
-    let mut loopstart = 0;
-    let mut past = k;
-    loop {
-        go = a[(go - 1) as usize];
-        count += 1;
-        if looped[(go - 1) as usize] == true {
-            // ここでループに入ったことを検知
-            if go != loopstart {
-                loopstart = go;
-            }
-            break
-        }
-        looped[(go - 1) as usize] = true;
-    }
+    let mut s = Vec::new();
+    let mut ord: Vec<i64> = vec![-1; n + 1]; // 何回目のジャンプでそこに辿りつくか
 
-    go = 1;
+    let mut go: usize = 1;
     loop {
-        if go == loopstart {
+        if ord[go] != -1 {
             break;
         }
-        go = a[(go - 1) as usize];
-        k -= 1;
-        count -= 1;
+        ord[go] = s.len() as i64;
+        s.push(go);
+        go = a[go - 1];
     }
-    if k < 0{
-        // ループにすらジャンプ回数が足りないときの処理
-        go = 1;
-        for _ in 0..past {
-            go = a[(go - 1) as usize];
-        }
-        println!("{}", go);
+    let loop_size = s.len() - ord[go] as usize; // 周期
+    let l = ord[go] as usize; // 例外部分の長さ
+
+    if k < l {
+        println!("{}", s[k]);
     } else {
-        go = loopstart;
-        for _ in 0..k % count {
-            go = a[(go - 1) as usize];
-        }
-        println!("{}", go);
+        k -= l;
+        k %= loop_size;
+        println!("{}", s[l + k]);
     }
 }
