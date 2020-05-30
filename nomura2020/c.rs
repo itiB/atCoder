1c6dee5f393d0056c5c6aef36af85002ad0cb74e
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-05-31 01:51:07 +0900

    AC: nomura2020-c

--- nomura2020/c.rs
+++ nomura2020/c.rs
@@ -4,29 +4,22 @@ fn main() {
     input! {
         n: usize,
         a: [f64; n],
-        a_last: f64
+        a_last: f64,
     }
 
     let mut sum = 0;
     let mut pred = 0.5;
-    let mut flag = 0.0;
-    let mut max = a.iter
+    let mut max = a.iter().fold(0.0, |s, i| s + i) + a_last;
 
     for stage in a {
+        max -= stage;
         pred = (pred * 2.0) - stage;
-        if pred <= 0.0 {
-            println!("-1");
-            return;
-        }
         if pred >= max {
             pred = max;
-            flag = stage;
         }
         sum += (pred + stage) as i64;
         // println!("stage:{}, pred: {}, sum: {}", stage, pred, sum);
     }
-
-    sum += flag as i64;
 
     if a_last > pred * 2.0 {
         println!("-1");
