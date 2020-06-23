19145cee9d66cb894dc9f7564c194bb98ad7d8d8
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-06-23 23:20:33 +0900

    reface: abc171-c

--- abc171/c.rs
+++ abc171/c.rs
@@ -6,16 +6,10 @@ fn main() {
     }
     let mut name = Vec::new();
     loop {
-        let mut number = n % 26;
-        if number == 0 {
-            number = 26;
-            n = n / 26 - 1;
-        } else {
-            n = n / 26;
-        }
-        name.push(number);
-
-        if n <= 0 { break }
+        n -= 1;
+        name.push(n % 26);
+        n /= 26;
+        if n == 0 { break }
     }
     for c in (0..name.len()).rev() {
         print!("{}", decoder(name[c]))
@@ -25,5 +19,5 @@ fn main() {
 
 fn decoder(input: i128) -> char {
     let a = 'a' as u8;
-    (a - 1 + input as u8) as char
+    (a + input as u8) as char
 }
