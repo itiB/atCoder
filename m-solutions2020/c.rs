84705f5f29903bf36f0f3cdba24d9299f6bd924b
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-07-25 22:54:23 +0900

    AC: m-solutions2020-c

--- m-solutions2020/c.rs
+++ m-solutions2020/c.rs
@@ -7,25 +7,11 @@ fn main() {
         a: [u128; n],
     }
 
-    let mut vec = Vec::new();
-
-    let mut point = 1;
-    for num in 0..k {
-        point *= a[num];
-    }
-    vec.push(point);
-
-    for num in k..n {
-        vec.push(vec[vec.len() - 1] / a[num - k] * a[num]);
-    }
-
-    let mut past_point = vec[0];
-    for num in 1..=n - k{
-        if past_point < vec[num] {
+    for num in 0..n - k{
+        if a[num] < a[num + k] {
             println!("Yes");
         } else {
             println!("No");
         }
-        past_point = vec[num];
     }
 }
