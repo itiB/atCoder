81b92c39896f03bd70c6b26f9331ddc76890d8b2
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-08-17 23:42:52 +0900

    AC: abc175-c

--- abc175/c.rs
+++ abc175/c.rs
@@ -1,41 +1,34 @@
 use proconio::input;
 
+// はじめにxは絶対値をとって共通して正にいることにする
+// 1. 往復にたどり着く前にKを使い切る
+//   - 答え: x - d * k
+//   - 制約: x - d * k > 0, d * k でオーバーフロー
+//      - x >= dk -> x / d > kで解決！
+// 2. 往復する
+//   - 往復にたどり着くまで: x / d
+//   - その後 -d, +d を繰り返す( k - x / d = k'回 -> 偶数or奇数)
+
 fn main() {
     input! {
-        mut x: i128,
-        mut k: usize,
-        d: i128,
+        mut x: i64,
+        mut k: i64,
+        d: i64,
     }
 
-    let mut last = 0;
-    let mut count = x/d;
+    x = x.abs();
 
-    if x > count * d {
-        x -= count *d;
+    if (x/d > k) {
+        // 往復にたどり着くまでにKを使い切る
+        println!("{}", x - d * k);
     } else {
-        x += 
-    }
-
-    for i in 0..k - count {
-        if (x + d).abs() > (x - d).abs() {
-            x -= d;
-            if last == -1 {
-                if (k - i + 1) % 2 != 0 {
-                    x += d;
-                }
-                break; 
-            }
-            last = 1;
-        } else {
-            x += d;
-            if last == 1 {
-                if (k - i + 1) % 2 != 0 {
-                    x -= d;
-                }
-                break;
-            }
-            last = -1;
+        // 往復し始めるのはe回目
+        let e = x / d;
+        k -= e;
+        x %= d;
+        if k % 2 == 1 {
+            x = (x - d).abs();
         }
+        println!("{}", x);
     }
-    println!("{}", x.abs());
 }
