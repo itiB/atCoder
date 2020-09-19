74dbe75767688e844065786f2f72f70a1acbf768
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-09-19 23:49:41 +0900

    refact: abc179-a

--- abc179/a.rs
+++ abc179/a.rs
@@ -1,21 +1,10 @@
 use proconio::input;
-use proconio::marker::Chars;
 
 fn main() {
     input! {
-        s: Chars,
+        s: String,
     }
 
-    let len = s.len();
-    if s[len - 1] == 's' {
-        for c in &s {
-            print!("{}", c);
-        }
-        println!("es");
-    } else {
-        for c in &s {
-            print!("{}", c);
-        }
-        println!("s");
-    }
+    println!("{}{}", s,
+        if s.chars().last().unwrap() == 's' { "es" } else { "s" } );
 }
