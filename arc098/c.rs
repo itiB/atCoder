8d9b58e0a8c18c0b078c1ebcb85fb68ad9036ac9
itiB <is0312vx@ed.ritsumei.ac.jp>
2020-11-16 21:33:18 +0900

    Add: arc098-c

--- arc098/c.rs
+++ arc098/c.rs
@@ -1,34 +1,25 @@
 use proconio::input;
-use proconio::marker::Chars;
-use std::cmp::min;
 
 fn main() {
     input! {
-        _: usize,
-        s: Chars,
+        n: usize,
+        mut r: [(usize, usize); n],
+        mut b: [(usize, usize); n],
     }
-
-    let mut map = Vec::new();
-
-    let mut num = 0;
-    for c in &s {
-        match c {
-            'E' => num += 1,
-            _ => {},
+    let mut ans = 0;
+    r.sort();
+    b.sort_by_key(|tuple| tuple.1);
+    for (c, d) in b {
+        let mut biggest = None;
+        for i in 0..n {
+            if r[i].0 < c && r[i].1 < d {
+                biggest = Some(i);
+            }
         }
-        map.push(num);
+        if let Some(i) = biggest {
+            r[i] = (201, 201);
+            ans += 1;
+        }
     }
-
-    let mut ans = 1_000_000;
-    for i in 0..s.len() {
-        let mut t = 0;
-        // リーダより右側でWを向いていない者たちの数
-        t += if i < s.len() - 2 { map[s.len() - 1] - map[i] } else { 0 };
-        // リーダより左側でEを向いていない者たちの数
-        t += if i > 1 { i - map[i - 1] } else { 0 };
-
-        ans = min(ans, t);
-    }
-
     println!("{}", ans);
 }
