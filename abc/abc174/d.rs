f1461fdeaf11e1ec6b8d861414a8bb38adc90608
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-06-04 00:02:56 +0900

    2nd: abc174-d

--- abc/abc174/d.rs
+++ abc/abc174/d.rs
@@ -3,28 +3,25 @@ use proconio::marker::Chars;
 
 fn main() {
     input! {
-        _: usize,
-        mut c: Chars,
+        n: usize,
+        c: Chars,
     }
 
-    let mut count = 0;
-    let mut i = 0; // 左端
-    let mut j = c.len() - 1; // それ以上先にRがない右端
-
-    while i < j && i < c.len() - 1{
-        // もし左端がWでなければ後ろから順にRを探しに行く
-        if c[i] == 'W' {
-            while j > i {
-                if c[j] == 'R' {
-                    c[i] = 'R';
-                    c[j] = 'W';
-                    count += 1;
+    let mut left = 0;
+    let mut right = n - 1;
+    let mut ans = 0;
+    while left < right && left < n {
+        if c[left] == 'W' {
+            while right > left {
+                if c[right] == 'R' {
+                    ans += 1;
+                    right -= 1;
                     break;
                 }
-                j -= 1;
+                right -= 1;
             }
         }
-        i += 1;
+        left += 1;
     }
-    println!("{}", count);
+    println!("{}", ans);
 }
