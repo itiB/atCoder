dc4fb42c9b8c0e65cebf0ad947baa6c38cee2542
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-06-05 23:22:28 +0900

    Refact: arc008-b

--- arc/arc008/b.rs
+++ arc/arc008/b.rs
@@ -9,14 +9,11 @@ fn main() {
         name: Chars,
         kit: Chars,
     }
-    let a_z = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let mut kits = a_z.chars().map(|c| (c, 0)).collect::<HashMap<_, i32>>();
-    let mut back = a_z.chars().map(|c| (c, 0)).collect::<HashMap<_, i32>>();
-    let mut names = a_z.chars().map(|c| (c, 0)).collect::<HashMap<_, i32>>();
+    let mut kits = HashMap::new();
+    let mut names = HashMap::new();
 
     for k in &kit {
         *kits.entry(*k).or_insert(0) += 1;
-        *back.entry(*k).or_insert(0) += 1;
     }
     for n in &name {
         *names.entry(*n).or_insert(0) += 1;
@@ -24,16 +21,17 @@ fn main() {
     let mut ans: i32 = 1;
 
     for c in &name {
-        if back.get(c).unwrap() == &0 {
+        if kits.contains_key(c) {
+            *kits.entry(*c).or_insert(0) -= 1;
+            if kits.get(c).unwrap() < &0 {
+                ans += 1;
+                for k in &kit {
+                    *kits.entry(*k).or_insert(0) += 1;
+                }
+            }
+        } else {
             ans = -1;
             break
-        }
-        *kits.entry(*c).or_insert(0) -= 1;
-        if kits.get(c).unwrap() < &0 {
-            ans += 1;
-            for k in &kit {
-                *kits.entry(*k).or_insert(0) += 1;
-            }
         }
     }
     println!("{}", ans);
