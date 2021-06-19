9964d22ac4fbf08967bd16d425612b51d9039a5f
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-06-19 18:55:20 +0900

    Refact: abc158-d

--- abc/abc158/d.rs
+++ abc/abc158/d.rs
@@ -5,43 +5,31 @@ use std::collections::VecDeque;
 fn main() {
     input! {
         s: Chars,
-        mut q: usize,
+        q: usize
     }
+    let mut ans: VecDeque<char> = VecDeque::from(s);
 
-    let mut queue = VecDeque::new();
-    for c in s {
-        queue.push_back(c);
-    }
-
-    let mut opposite = false;
+    let mut rev = false;
     for _ in 0..q {
-        input! {
-            t: usize,
-        }
-        if t == 1 {
-            opposite = !opposite;
-        } else {
+        input! {t: usize}
+        if t == 2 {
             input! {
                 f: usize,
                 c: char,
             }
-            if (f == 1 && !opposite) || (f != 1 && opposite) {
-                queue.push_front(c);
+            if rev && f == 2 || !rev && f == 1 {
+                ans.push_front(c);
             } else {
-                queue.push_back(c);
+                ans.push_back(c);
             }
+        } else {
+            rev ^= true;
         }
     }
-    if !opposite {
-        // while queue.len() > 0 {
-        //     print!("{}", queue.pop_front().unwrap());
-        // }
-        println!("{}", queue.iter().collect::<String>());
+
+    if !rev {
+        println!("{}", ans.iter().collect::<String>());
     } else {
-        // while queue.len() > 0 {
-        //     print!("{}", queue.pop_back().unwrap());
-        // }
-        println!("{}", queue.iter().rev().collect::<String>());
+        println!("{}", ans.iter().rev().collect::<String>());
     }
-    // println!("");
 }
