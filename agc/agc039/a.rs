685a9a525d2c48c69aa1fa22543014ce8c63d26b
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-06-29 08:28:38 +0900

    2nd: agc039-a

--- agc/agc039/a.rs
+++ agc/agc039/a.rs
@@ -1,50 +1,46 @@
-use std::io;
+use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
-	let s = read::<String>();
-	let k = read::<i64>();
+    input! {
+        s: Chars,
+        k: usize
+    }
 
-	// 入力された文字列を2回繰り返した文字列の作成
-	let mut count: i64 = 0;
+    let mut ans = 0;
+    let mut ss = s.clone();
 
-	// 初回の値を取得
-	let (num_1, last_char) = ret_char_num(&s);
-
-	// 偶数回目の値を取得
-	let first_s: String = format!("{}{}", last_char, s);
-	let (num_2, last_char) = ret_char_num(&first_s);
-
-	// 奇数回目の値を取得
-	let mids_s: String = format!("{}{}", last_char, s);
-	let (num_3, _last_char) = ret_char_num(&mids_s);
-
-	if k > 0{
-		count = num_1 + (num_2 + num_3) * (k / 2);
-		if k % 2 == 0 {
-			count = count - num_3;
-		}
-	}
-	println!("{}", count);
-}
-
-fn read<T: std::str::FromStr>() -> T {
-	let mut line = String::new();
-	io::stdin().read_line(&mut line).ok();
-	line.trim().parse().ok().unwrap()
-}
-
-fn ret_char_num(s : &String) -> (i64, char) {
-	// 渡された文字列の連続する文字の数を返す
-
-	let mut vec: Vec<char> = s.as_str().chars().collect();
-	let mut count: i64 = 0;
-
-	for i in 1..vec.len(){
-		// 1つ前と一致した場合，存在しない文字に書き換える
-		if vec[i - 1] == vec[i]{
-			count = count + 1;
-			vec[i] = '!';
-		}
-	}
-	return (count, vec[vec.len() - 1]);
+    if s[0] != s[s.len() - 1] {
+        // はじめと最後が違う文字 -> そのまま掛け算
+        for i in 1..ss.len() {
+            if ss[i - 1] == ss[i] {
+                ans += 1;
+                ss[i] = '.';
+            }
+        }
+        ans *= k;
+    } else {
+        ss.append(&mut s.clone());
+        let mut mid = 0;
+        for i in 1..ss.len() {
+            if ss[i - 1] == ss[i] {
+                ans += 1;
+                if i < s.len() { mid += 1; }
+                ss[i] = '.';
+            }
+        }
+        if ss[0] != ss[ss.len() - 1] {
+            // 2回まとめたらそのままつなげられる -> 奇数回なら調整
+            ans *= k / 2;
+            if k % 2 == 1 {
+                ans += mid
+            }
+        } else {
+            // 2回目以降は全部同じ数が足されるパターン
+            ans -= mid;
+            ans *= k - 1;
+            ans += mid;
+        }
+    }
+    println!("{}", ans);
 }
