71552e736e4253a362572e4991ab86c45093b110
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-06-28 23:55:21 +0900

    2nd: abc166-d

--- abc/abc166/d.rs
+++ abc/abc166/d.rs
@@ -2,14 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i64,
+        x: i64,
     }
 
-    for a in -119..=120 {
-        for b in -119..=120 {
-            if (a as i64).pow(5) - (b as i64).pow(5) == n {
+    for a in 0..200 {
+        let a5 = a * a * a * a * a;
+        for b in -200..200 {
+            let b5 = b * b * b * b * b;
+            if a5 - b5 == x {
                 println!("{} {}", a, b);
-                return
+                return;
             }
         }
     }
