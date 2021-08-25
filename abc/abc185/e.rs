9b882f04dc276c0720cfd6e752073f29ccade1d5
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-08-25 09:51:40 +0900

    Add: abc185-c

--- abc/abc185/e.rs
+++ abc/abc185/e.rs
@@ -1,33 +1,22 @@
 use proconio::input;
 use std::cmp::min;
+
 fn main() {
     input! {
         n: usize,
         m: usize,
         a: [usize; n],
-        b: [usize; m],
+        b: [usize; m]
     }
 
-    let mut ab = vec![vec![0; m + 1]; n + 1];
-    for i in 0..=n {
-        ab[i][0] = i;
-    }
-    for i in 0..=m {
-        ab[0][i] = i;
-    }
-    for y in 0..m {
-        for x in 0..n {
-            if a[x] != b[y] {
-                // ab[x + 1][y + 1] = max(ab[x + 1][y], ab[x][y + 1]);
-                ab[x + 1][y + 1] = min(min(ab[x][y] + 1, ab[x + 1][y] + 1), ab[x][y + 1] + 1);
-            } else {
-                ab[x + 1][y + 1] = ab[x][y]
-            }
+    let mut dp = vec![vec![0; m + 1]; n + 1];
+    for i in 0..=n { dp[i][0] = i }
+    for i in 0..=m { dp[0][i] = i }
+    for i in 1..=n {
+        for j in 1..=m {
+            let cost = if a[i-1] != b[j-1] { 1 } else { 0 };
+            dp[i][j] = min(dp[i-1][j]+1, min(dp[i][j-1]+1, dp[i-1][j-1]+cost));
         }
     }
-
-    // for aa in ab {
-    //     println!("{:?}", aa);
-    // }
-    println!("{}", ab[n][m]);
+    println!("{}", dp[n][m])
 }
