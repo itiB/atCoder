613ac025642d18193541f9c7a00eb65bcd101a91
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-08-27 00:21:56 +0900

    2nd: dp-a

--- lesson/dp/a.rs
+++ lesson/dp/a.rs
@@ -4,20 +4,13 @@ use std::cmp::min;
 fn main() {
     input! {
         n: usize,
-        h: [i32; n],
+        h: [i64; n]
     }
 
     let mut dp = vec![0; n];
-
-    for i in 1..n {
-        if i > 1 {
-            // i-1 か i-2 からしか来ることはない
-            dp[i] = min(dp[i - 1] + (h[i - 1] - h[i]).abs(), 
-                        dp[i - 2] + (h[i - 2] - h[i]).abs())
-        } else {
-            // i-2 < 1 のときに i-2 はないので i-1のみ
-            dp[i] = dp[i - 1] + (h[i - 1] - h[i]).abs();
-        }
+    dp[1] = (h[1] - h[0]).abs();
+    for i in 2..n {
+        dp[i] = min(dp[i-1]+(h[i] - h[i-1]).abs(), dp[i-2]+(h[i] - h[i-2]).abs());
     }
-    println!("{}", dp[n - 1]);
+    println!("{}", dp[n-1]);
 }
