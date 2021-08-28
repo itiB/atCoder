4f6626ca1ca01e0508d24e26311b231e9670e1b6
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-08-29 01:33:24 +0900

    2nd: dp-d

--- lesson/dp/d.rs
+++ lesson/dp/d.rs
@@ -5,23 +5,30 @@ fn main() {
     input! {
         n: usize,
         w: usize,
-        wv: [(usize, usize); n],
+        wv: [(usize, usize); n]
     }
 
-    let mut dp = vec![vec![0; w + 1]; n + 1];
-
-    for i in 0..n {
-        for j in 0..=w {
-            if j >= wv[i].0 {
-                // 現在の荷物量jの中で最大の価値を生み出すものをdp[i][j]に入れる
-                // 1, 選ばなかった時: dp[i-1][j]の価値
-                // 2. 選んだとき    : dp[i-1][j - 今から増やす重さの位置] + 増やす価値
-                dp[i + 1][j] = max(dp[i][j], dp[i][j - wv[i].0] + wv[i].1);
-            } else {
-                // 荷物が重すぎて入らない場合は左隣の価値をそのまま引きつぐ
-                dp[i + 1][j] = dp[i][j];
+    let mut dp = vec![vec![None; w+1]; n+1];
+    dp[0][0] = Some(0);
+    for i in 1..=n {
+        for j in (0..=w).rev() {
+            if let Some(v) = dp[i-1][j] {
+                dp[i][j] = Some(v);
+                if j + wv[i - 1].0 <= w {
+                    let tmp = dp[i][j].unwrap() + wv[i-1].1;
+                    dp[i][j + wv[i - 1].0] = match dp[i][j + wv[i - 1].0] {
+                        None => Some(tmp),
+                        Some(j) => Some(max(j, tmp)),
+                    }
+                }
             }
         }
     }
-    println!("{}", dp[n][w]);
+    let mut ans = 0;
+    for i in &dp[n] {
+        if let Some(v) = i {
+            ans = max(ans, *v);
+        }
+    }
+    println!("{}", ans);
 }
