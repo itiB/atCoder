978f523cfae22a6825f87ebe27208cd5ffe66cff
itiB <is0312vx@ed.ritsumei.ac.jp>
2021-08-31 00:05:53 +0900

    2nd: dp-f

--- lesson/dp/f.rs
+++ lesson/dp/f.rs
@@ -8,34 +8,37 @@ fn main() {
         t: Chars,
     }
 
-    let mut dp = vec![vec![0; s.len() + 1]; t.len() + 1];
-
-    for y in 1..=t.len() {
-        for x in 1..=s.len() {
-            if s[x - 1] == t[y - 1] {
-                dp[y][x] = dp[y - 1][x - 1] + 1;
+    let mut dp = vec![vec![0; s.len()+1]; t.len()+1];
+    for j in 1..=t.len() {
+        for i in 1..=s.len() {
+                if s[i-1] == t[j-1] {
+                dp[j][i] += dp[j-1][i-1] + 1;
             } else {
-                dp[y][x] = max(dp[y][x - 1], dp[y - 1][x]);
+                dp[j][i] = max(dp[j-1][i], dp[j][i-1]);
             }
         }
     }
-    let mut x = s.len();
-    let mut y = t.len();
-    let mut l = dp[y][x];
-    let mut ans = Vec::new();
+
+    let mut l = dp[t.len()][s.len()];
+    let mut ans = vec![None; l+1];
+    let mut i = s.len();
+    let mut j = t.len();
     while l > 0 {
-        // 逆にたどって文字列を調べる
-        if s[x - 1] == t[y - 1] {
-            ans.push(s[x - 1]);
-            x -= 1;
-            y -= 1;
+        if s[i - 1] == t[j - 1] {
+            ans[l] = Some(s[i-1]);
             l -= 1;
-        } else if dp[y][x] == dp[y][x - 1] {
-            x -= 1;
+            i -= 1;
+            j -= 1;
+        } else if dp[j][i] == dp[j][i-1] {
+            i -= 1;
         } else {
-            y -= 1;
+            j -= 1;
         }
     }
-    let s: String = ans.into_iter().rev().collect();
-    println!("{}", s);
+    for c in ans {
+        if let Some(v) = c {
+            print!("{}", v);
+        }
+    }
+    println!("");
 }
